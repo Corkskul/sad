61f88928c24fddcc9ea1c77a6ef1edfbb5da6f3e
Corkskul <diego.sainz.sanchez@gmail.com>
2023-03-01 12:22:56 +0100

    Co-authored-by: IgorYm2020 <IgorYm2020@users.noreply.github.com>

--- src/main/java/bufferedreader/Line.java
+++ src/main/java/bufferedreader/Line.java
@@ -2,13 +2,6 @@ package bufferedreader;
 
 public class Line {
     StringBuilder sb = new StringBuilder();
-    char c;
-
-    public Line(char c) {
-
-        this.c = c;
-
-    }
 
     public void add(char c) {
         System.out.print((char) c);
